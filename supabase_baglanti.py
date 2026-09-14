69d9a17f5c70e602a40d8c4c00751475e2c8740a
Mahmut Unal <mahmut@example.com>
2026-09-14 13:28:08 +0300

    Güvenlik ayarlari eklendi

--- supabase_baglanti.py
+++ supabase_baglanti.py
@@ -1,7 +1,6 @@
+import streamlit as st
 from supabase import create_client, Client
 
-SUPABASE_URL = "https://obgsuwmljpvniincpkvr.supabase.co"
-SUPABASE_KEY = "sb_publishable_i1KMuRCv2rsa9_dvse8MMw_h7s8TEvR"
-
-# Supabase istemcisini başlatıyoruz
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
